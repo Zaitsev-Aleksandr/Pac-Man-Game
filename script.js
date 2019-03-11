0be1478aa533b7a6f21ef000777315f6e2e7835c
andrey <yurist.feo@gmail.com>
2019-03-11 12:28:46 +0200

    wiht bot

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ let packMan = document.getElementById('packMan');
 let offSetLeftFild = document.getElementsByClassName('packMan-Fild')[0].offsetWidth;
 let offSetTopFild = document.getElementsByClassName('packMan-Fild')[0].offsetHeight;
 let i = 'left';
-
-
+let Total = document.getElementById('total');
+let Bot;
+Total.value = 0;
 document.onkeydown = function () {
 
     switch (event.keyCode) {
-
         case 37 :
             clearInterval(i);
             i = 'left';
             packMan.removeAttribute("class");
             packMan.classList.add("move-left");
             i = setInterval((() => (packMan.offsetLeft > 0 ?
-                packMan.style.left = packMan.offsetLeft - 1 + "px" : clearInterval(i))), 10);
+                packMan.style.left = packMan.offsetLeft - 1 + "px" : clearInterval(i))), 3);
             break;
         case 38:
             clearInterval(i);
@@ -22,7 +22,7 @@ document.onkeydown = function () {
             packMan.removeAttribute("class");
             packMan.classList.add("move-top");
             i = setInterval((() => (packMan.offsetTop > 0 ?
-                packMan.style.top = packMan.offsetTop - 1 + "px" : clearInterval(i))), 10);
+                packMan.style.top = packMan.offsetTop - 1 + "px" : clearInterval(i))), 3);
 
             break;
         case 39:
@@ -31,7 +31,7 @@ document.onkeydown = function () {
             packMan.removeAttribute("class");
             packMan.classList.add("move-right");
             i = setInterval((() => (packMan.offsetLeft < (offSetLeftFild - packMan.offsetWidth) ?
-                packMan.style.left = packMan.offsetLeft + 1 + "px" : clearInterval(i))), 10);
+                packMan.style.left = packMan.offsetLeft + 1 + "px" : clearInterval(i))), 3);
             break;
         case 40:
             clearInterval(i);
@@ -39,34 +39,114 @@ document.onkeydown = function () {
             packMan.removeAttribute("class");
             packMan.classList.add("move-bottom");
             i = setInterval((() => (packMan.offsetTop < (offSetTopFild - packMan.offsetHeight) ?
-                packMan.style.top = packMan.offsetTop + 1 + "px" : clearInterval(i))), 10);
+                packMan.style.top = packMan.offsetTop + 1 + "px" : clearInterval(i))), 3);
             break;
-        case 32:
-            clearInterval(i);
-            i = 'left';
-            break;
-        case 16:
-            newFruit();
-            break;
+
     }
 };
-let arrSrc=["img/lollipop.png","img/burger.png",
-    "img/candy.png","img/corn.png","img/fish.png","img/gingerbread-man.png",
-    "img/lobster.png","img/pear.png","img/tomato.png"]
+
+//  ___________________building fruit_______________________________________-
+let arrSrc = ["img/lollipop.png", "img/burger.png",
+    "img/candy.png", "img/corn.png", "img/fish.png", "img/gingerbread-man.png",
+    "img/lobster.png", "img/pear.png", "img/tomato.png"];
 let fruits;
 let newFruit = function () {
     let newImg = document.createElement('img');
-    newImg.className = 'fruit';
+    newImg.className = 'fruit';
     newImg.src = arrSrc[Math.round(Math.random() * (8 - 0))];
-    newImg.style.top=Math.round(Math.random() * ((offSetTopFild -(packMan.clientWidth))- 0))+"px";
-    newImg.style.left =Math.round(Math.random() * ((offSetLeftFild-(packMan.clientHeight)) - 0))+"px";
+    newImg.style.top = Math.round(Math.random() * ((offSetTopFild - (packMan.clientWidth)) - 0)) + "px";
+    newImg.style.left = Math.round(Math.random() * ((offSetLeftFild - (packMan.clientHeight)) - 0)) + "px";
     document.getElementsByClassName('packMan-Fild')[0].appendChild(newImg);
-    fruits= document.getElementsByClassName('fruit')[0];
+    fruits = document.getElementsByClassName('fruit')[0];
 };
+//  __________counting the fruits eaten______________________________
 let newStart = function () {
+
     document.getElementsByClassName('packMan-Fild')[0].removeChild(fruits);
+    Total.value = +Total.value + 1;
     newFruit();
 };
-let g=setInterval((() => (Math.abs(packMan.offsetLeft - fruits.offsetLeft) < 20
-&& Math.abs(packMan.offsetTop - fruits.offsetTop) < 20) ? newStart() : false), 50);
+
+
+setInterval((() => (Math.abs(packMan.offsetLeft - fruits.offsetLeft) < 25
+    && Math.abs(packMan.offsetTop - fruits.offsetTop) < 25) ? newStart() : false), 50);
+
+// ____________________________BOT pakMen______________________________________________
+let createBot = function () {
+    Bot = packMan.cloneNode(true);
+    Bot.id = "BotPakMen";
+    packMan.removeAttribute("class");
+    packMan.classList.add("move-right");
+    Bot.style.top = offSetTopFild - packMan.clientWidth + "px";
+    Bot.style.left = offSetLeftFild + "px";
+    packMan.parentNode.insertBefore(Bot, packMan.nextSibling);
+    Bot = document.getElementById("BotPakMen");
+};
+
+//__________________________________   move BOT__________________________
+
+
+let b = 'right';
+let movesBot = function (coordinateDiff) {
+
+    switch (coordinateDiff) {
+        case 'left' :
+            clearInterval(b);
+            b = 'left';
+            Bot.removeAttribute("class");
+            Bot.classList.add("move-left");
+            b = setInterval((() => (Bot.offsetLeft > 0 ?
+                Bot.style.left = Bot.offsetLeft - 1 + "px" : clearInterval(b))), 6);
+            break;
+        case 'top':
+            clearInterval(b);
+            b = 'top';
+            Bot.removeAttribute("class");
+            Bot.classList.add("move-top");
+            b = setInterval((() => (Bot.offsetTop > 0 ?
+                Bot.style.top = Bot.offsetTop - 1 + "px" : clearInterval(b))), 6);
+            break;
+        case 'right':
+            clearInterval(b);
+            b = 'right';
+            Bot.removeAttribute("class");
+            Bot.classList.add("move-right");
+            b = setInterval((() => (Bot.offsetLeft < (offSetLeftFild - Bot.offsetWidth) ?
+                Bot.style.left = Bot.offsetLeft + 1 + "px" : clearInterval(b))), 6);
+            break;
+        case 'bottom':
+            setInterval(b);
+            b = 'bottom';
+            Bot.removeAttribute("class");
+            Bot.classList.add("move-bottom");
+            b = setInterval((() => (Bot.offsetTop < (offSetTopFild - Bot.offsetHeight) ?
+                Bot.style.top = Bot.offsetTop + 1 + "px" : clearInterval(b))), 6);
+            break;
+    }
+};
+setInterval(function () {
+        clearInterval(b);
+        if (Math.abs(packMan.offsetLeft - Bot.offsetLeft) < 50 &&
+            Math.abs(packMan.offsetTop - Bot.offsetTop) < 50) {
+            document.getElementsByClassName('packMan-Fild')[0].removeChild(packMan);
+            }
+        else if (packMan.offsetLeft - Bot.offsetLeft < 0 && packMan.offsetTop - Bot.offsetTop < 100) {
+            movesBot("left")
+        }
+        else if (packMan.offsetTop - Bot.offsetTop < 0 && packMan.offsetLeft - Bot.offsetLeft < 100) {
+            movesBot("top")
+        }
+        else if (packMan.offsetLeft - Bot.offsetLeft > 0 && packMan.offsetTop - Bot.offsetTop < 100) {
+            movesBot("right")
+        }
+        else if (packMan.offsetTop - Bot.offsetTop > 0 && packMan.offsetLeft - Bot.offsetLeft < 100) {
+            movesBot("bottom")
+        }
+    },
+    150
+);
+
+
+
 newFruit();
+createBot();
